fb8c5afe917c0e4fb1d8b3763326553919cdc887
Matt Doak <matt.doak@reclaim.io>
2014-11-12 04:28:25 +0000

    Changed the title and some settings in the slider

--- js/slider/main.js
+++ js/slider/main.js
@@ -2,12 +2,12 @@ $(window).load(function(){
 	$('.slider').fractionSlider({
 		'fullWidth': 			true,
 		'controls': 			true, 
-		'pager': 				true,
+		'pager': 				false,
 		'responsive': 			true,
 		'dimensions': 			"1100,550",
+		'timeout': 3000,
 	    'increase': 			false,
 		'pauseOnHover': 		false,
 		'slideEndAnimation': 	true
 	});
-
 });
